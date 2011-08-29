7f5dd5028172cf542dc0b14f666c136a636a231e
Drew Walton <drewwalton19216801@gmail.com>
2011-08-29 09:56:56 -0600

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 5 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 PREEMPT Mon Aug 29 09:36:49 MDT 2011"
+#define LINUX_COMPILE_TIME "09:36:49"
+#define LINUX_COMPILE_BY "lg-electronics"
+#define LINUX_COMPILE_HOST "drewwalton-MS-7549"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.5.2 (Ubuntu/Linaro 4.5.2-8ubuntu3) "
