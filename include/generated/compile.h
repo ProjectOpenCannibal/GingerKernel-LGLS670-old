e2fc815194bf922c5d4c39e38905e379f26f2ab5
Drew Walton <drewwalton19216801@gmail.com>
2011-09-06 16:44:12 -0600

    Revert overclocking, touchscreen changes

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Mon Aug 29 12:29:08 MDT 2011"
-#define LINUX_COMPILE_TIME "12:29:08"
+#define UTS_VERSION "#2 PREEMPT Mon Aug 29 13:11:17 MDT 2011"
+#define LINUX_COMPILE_TIME "13:11:17"
 #define LINUX_COMPILE_BY "lg-electronics"
 #define LINUX_COMPILE_HOST "drewwalton-MS-7549"
 #define LINUX_COMPILE_DOMAIN "(none)"
