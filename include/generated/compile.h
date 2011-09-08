007dd2d267191fc87ab683b5fb3cb18a0e80b8a0
Drew Walton <drewwalton19216801@gmail.com>
2011-09-08 09:28:03 -0600

    Double fail

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 6 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 PREEMPT Tue Sep 6 18:22:10 MDT 2011"
-#define LINUX_COMPILE_TIME "18:22:10"
+#define UTS_VERSION "#6 PREEMPT Thu Sep 8 09:15:13 MDT 2011"
+#define LINUX_COMPILE_TIME "09:15:13"
 #define LINUX_COMPILE_BY "lg-electronics"
 #define LINUX_COMPILE_HOST "drewwalton-MS-7549"
 #define LINUX_COMPILE_DOMAIN "(none)"
