a96a70b89242735b927f16c79c21bdbd8b6f4d21
Drew Walton <drewwalton19216801@gmail.com>
2011-09-15 06:57:10 -0600

    Make GSPCA_MAIN built-in

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 1 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 PREEMPT Thu Sep 8 09:15:13 MDT 2011"
-#define LINUX_COMPILE_TIME "09:15:13"
+#define UTS_VERSION "#1 PREEMPT Thu Sep 15 06:52:12 MDT 2011"
+#define LINUX_COMPILE_TIME "06:52:12"
 #define LINUX_COMPILE_BY "lg-electronics"
 #define LINUX_COMPILE_HOST "drewwalton-MS-7549"
 #define LINUX_COMPILE_DOMAIN "(none)"
