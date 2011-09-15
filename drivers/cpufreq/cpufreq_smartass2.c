90a86f8f627f64c583231f47218ea0957a03ec7b
Drew Walton <drewwalton19216801@gmail.com>
2011-09-15 07:22:57 -0600

    Smartass2 tweaks

--- drivers/cpufreq/cpufreq_smartass2.c
+++ drivers/cpufreq/cpufreq_smartass2.c
@@ -44,7 +44,7 @@
  * twards the ideal frequency and slower after it has passed it. Similarly,
  * lowering the frequency twards the ideal frequency is faster than below it.
  */
-#define DEFAULT_AWAKE_IDEAL_FREQ 806400
+#define DEFAULT_AWAKE_IDEAL_FREQ 652800
 static unsigned int awake_ideal_freq;
 
 /*
@@ -53,7 +53,7 @@ static unsigned int awake_ideal_freq;
  * that practically when sleep_ideal_freq==0 the awake_ideal_freq is used
  * also when suspended).
  */
-#define DEFAULT_SLEEP_IDEAL_FREQ 480000
+#define DEFAULT_SLEEP_IDEAL_FREQ 320000
 static unsigned int sleep_ideal_freq;
 
 /*
@@ -98,7 +98,7 @@ static unsigned long down_rate_us;
  * The frequency to set when waking up from sleep.
  * When sleep_ideal_freq=0 this will have no effect.
  */
-#define DEFAULT_SLEEP_WAKEUP_FREQ 806400
+#define DEFAULT_SLEEP_WAKEUP_FREQ 652800
 static unsigned int sleep_wakeup_freq;
 
 /*
